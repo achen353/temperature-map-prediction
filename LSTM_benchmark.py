71ae8afc52c69c5c46718d42ea529b3d595dd3a2
tyy-cc <cchen79601@gmail.com>
2022-04-15 16:12:49 -0400

    update transformer and add an LSTM benchmark model

--- LSTM_benchmark.py
+++ LSTM_benchmark.py
@@ -2,17 +2,15 @@ import torch
 import torch.nn as nn
 
 
-class Transformer(nn.Module):
+class LSTM(nn.Module):
     def __init__(
         self,
-        num_enc_layers,
+        num_layers,
         input_size,
-        input_len,
         pred_len,
         feature_size,
-        NHEAD,
-        has_pos=False,
-        dropout=0.1,
+        dropout=0,
+        bidirectional = False
     ):
         super(Transformer, self).__init__()
         # num_enc_layers (hyperparameter): number of the encoder layers
@@ -23,21 +21,12 @@ class Transformer(nn.Module):
         # NHEAD: number of heads in the transformer encoder
         # has_pos: has position embedding or not
         # dropout: dropout of the transformer encoder
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.has_pos = has_pos
-        self.project_linear = nn.Linear(
-            input_size, feature_size
-        )  # project input to the feature_size
 
-        self.encoder_layer = nn.TransformerEncoderLayer(
-            d_model=feature_size, nhead=NHEAD, dropout=dropout
-        )
-        self.transformer_enc = nn.TransformerEncoder(
-            self.encoder_layer3, num_layers=num_enc_layers
-        )
+        self.input_size = input_size
+        self.pred_len = pred_len
+        self.lstm = nn.LSTM(input_size = self.input_size, hidden_size = feature_size, num_layers = num_layers, dropout = dropout, bidirectional = bidirectional)
 
-        if self.has_pos:
-            self.pos_layer = nn.Linear(input_size, input_size)
+
 
         self.linear1 = nn.Linear(feature_size, feature_size)
         self.l_relu1 = nn.LeakyReLU()
@@ -58,14 +47,8 @@ class Transformer(nn.Module):
         self.linear3.bias.data.zero_()
         self.linear3.weight.data.uniform_(-range, range)
 
-        self.project_linear.bias.data.zero_()
-        self.project_linear.weight.data.uniform_(-range, range)
 
-        if self.has_pos:
-            self.pos_layer.bias.data.zero_()
-            self.pos_layer.weight.data.uniform_(-range, range)
-
-    def forward(self, input, pos_info=None, token_is_zero=True):
+    def forward(self, input):
 
         # input: shape is [seq_len, batch_size, input_size]
         # pos_info: shape is [seq_len + pred_len, batch_size, 1]
@@ -73,27 +56,11 @@ class Transformer(nn.Module):
 
         seq_len, batch_size, input_size = input.shape
 
-        if token_is_zero:
-            tokens = torch.zeros(
-                self.pred_len, batch_size, input_size
-            )  # this is for the output seq
-        else:
-            tokens = input[-1, :, :]
-            tokens = tokens.repeat([self.pred_len, 1, 1])
 
-        output = torch.cat((input, tokens.to(self.device)), 0)  # cat two tensors
 
-        if self.has_pos:
-            pos_enc = self.pos_layer(
-                output
-            )  # using linear transform as the position embedding
-            output = torch.add(pos_enc, output)  # add position info and input
+        output, (h_n, c_n) = self.lstm(input, None)
 
-        output = self.project_linear(output)
-
-        output = self.transformer_enc(output)
-
-        output = output[input_size:, :, :]  # only use the sequence of tokens to predict
+        output = output[-self.pred_len:, :, :]
 
         output = self.linear1(output)
         output = self.l_relu1()
@@ -108,8 +75,7 @@ class Transformer(nn.Module):
 ################ example to train the model ####################
 ###############################################################
 
-# model = Transformer(num_enc_layers=3, input_size=400, input_len=20, pred_len=5, feature_size = 512, NHEAD=4, has_pos=True).to(device)
-# pos_info = [[1], [2], ..., [25]] # assummed
+# model = LSTM(num_layers=3, input_size=400, pred_len=5, feature_size = 512, dropout=0, bidirectional=True).to(device)
 # criterion = nn.MSELoss()
 # optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 # scheduler = StepLR(optimizer, step_size=100, gamma=0.99)
@@ -123,7 +89,7 @@ class Transformer(nn.Module):
 #
 #         y = y.permute(1,0,2) # make sure batch size is in the second dim
 #
-#         pred = model(x, pos_info, token_is_zero = False)
+#         pred = model(x)
 #       ............
 
 
