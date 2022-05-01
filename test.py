aa889e277f4f9774cfb390b380cfa1d63ccd60aa
Andrew Chen <achen353@gatech.edu>
2022-05-02 07:17:53 +0800

    Add experiment results (#9)
    * fix nan bug
    
    * option for temp diff
    
    * fix pos encoding
    
    * fix minor model bugs
    
    * update train script
    
    * Automated reformatting
    
    * fix bugs
    
    * add experiments
    
    * add results
    
    * Automated reformatting
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,109 @@
+import argparse
+import json
+import os
+import random
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from modules.dataloader_v2 import MODISDataset
+from modules.LSTM_benchmark import LSTM
+from modules.transformer_map_pred import Transformer
+
+MODELS = {"Transformer": Transformer, "LSTM": LSTM}
+
+
+def main(config_path):
+    try:
+        print("[INFO] Loading config from: {}".format(config_path))
+        config = json.load(open(config_path))
+    except:
+        raise FileNotFoundError("Config {} does not exist.".format(config_path))
+
+    if config["seed"]["fixed"]:
+        seed = config["seed"]["value"]
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+
+    model_class = MODELS[config["model"]["class"]]
+    loss_class = getattr(nn, config["loss"])
+
+    has_pos_info = "enc" in config["name"]
+    token_is_zero = "ulast" not in config["name"]
+    use_diff = config["dataset"]["hyperparams"]["use_diff"]
+
+    model = model_class(**config["model"]["hyperparams"])
+
+    if "device" in config:
+        model.device = "cuda:5"
+    device = model.device
+    model.to(model.device)
+
+    resume_from_checkpoint = os.path.join(
+        "model_checkpoints/", config["resume_from_checkpoint"], "best/best.pt"
+    )
+    checkpoint = torch.load(resume_from_checkpoint, map_location=torch.device(device))
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.eval()
+
+    criterion = loss_class()
+
+    train_dataset = MODISDataset(**config["dataset"]["hyperparams"])
+    collate_fn = train_dataset.collate
+
+    test_dataset = MODISDataset(**config["dataset"]["hyperparams"], mode="test")
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=config["dataloader"]["batch_size"],
+        shuffle=True,
+        collate_fn=collate_fn,
+        pin_memory=True,
+    )
+
+    # Test
+    t_losses = []
+    for i, (t_input, t_pred, t_encodings) in enumerate(test_dataloader):
+        t_input, t_pred, t_encodings = (
+            t_input.permute(1, 0, 2),
+            t_pred.permute(1, 0, 2),
+            t_encodings.permute(1, 0).unsqueeze(dim=-1),
+        )
+
+        t_input, t_pred, t_encodings = (
+            t_input.to(device),
+            t_pred.to(device),
+            t_encodings.to(device),
+        )
+
+        if model_class == Transformer:
+            if has_pos_info:
+                t_output = model(
+                    t_input, pos_info=t_encodings, token_is_zero=token_is_zero
+                )
+            else:
+                t_output = model(t_input, pos_info=None, token_is_zero=token_is_zero)
+        else:
+            t_output = model(t_input)
+
+        if use_diff:
+            t_output = torch.cumsum(t_output, dim=0)
+            t_pred = torch.cumsum(t_pred, dim=0)
+
+        t_loss = criterion(t_output, t_pred)
+        t_loss_val = t_loss.item()
+        t_losses.append(t_loss_val)
+
+    test_loss = np.mean(t_losses)
+
+    print("[INFO] Test Loss: {}".format(test_loss))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config_name", type=str, default="transformer.json")
+    args = parser.parse_args()
+    config_path = os.path.join("configs", args.config_name)
+    main(config_path)
--- train.py
+++ train.py
@@ -18,7 +18,7 @@ from modules.transformer_map_pred import Transformer
 MODELS = {"Transformer": Transformer, "LSTM": LSTM}
 
 
-def main(config_path):
+def main(config_path, trial_num=0):
     try:
         print("[INFO] Loading config from: {}".format(config_path))
         config = json.load(open(config_path))
@@ -31,15 +31,22 @@ def main(config_path):
         np.random.seed(seed)
         torch.manual_seed(seed)
 
+    RUN_NAME = "{}_{}".format(config["name"], trial_num)
     EPOCH = config["epoch"]
     model_class = MODELS[config["model"]["class"]]
     optimizer_class = getattr(optim, config["optimizer"]["name"])
     scheduler_class = getattr(lr_scheduler, config["scheduler"]["name"])
     loss_class = getattr(nn, config["loss"])
 
-    model = model_class(**config["model"]["hyperparams"])
-    device = model.device
+    has_pos_info = "enc" in config["name"]
+    token_is_zero = "ulast" not in config["name"]
+    use_diff = config["dataset"]["hyperparam"]["use_diff"]
 
+    model = model_class(**config["model"]["hyperparams"])
+
+    if "device" in config:
+        model.device = config["device"]
+    device = model.device
     model.to(model.device)
 
     criterion = loss_class()
@@ -47,6 +54,10 @@ def main(config_path):
     scheduler = scheduler_class(
         optimizer=optimizer, **config["scheduler"]["hyperparams"]
     )
+
+    if not os.path.exists("./model_checkpoints/{}".format(RUN_NAME)):
+        os.makedirs("./model_checkpoints/{}/all".format(RUN_NAME))
+        os.makedirs("./model_checkpoints/{}/best".format(RUN_NAME))
 
     train_dataset = MODISDataset(**config["dataset"]["hyperparams"])
     collate_fn = train_dataset.collate
@@ -68,12 +79,27 @@ def main(config_path):
         pin_memory=True,
     )
 
+    test_dataset = MODISDataset(**config["dataset"]["hyperparams"], mode="test")
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=config["dataloader"]["batch_size"],
+        shuffle=True,
+        collate_fn=collate_fn,
+        pin_memory=True,
+    )
+
     writer = SummaryWriter(comment=config["name"])
+
+    n_iter = 0
+    best_valid_loss, best_test_loss = float("inf"), float("inf")
 
     for epoch in range(EPOCH):
         losses = []
-        model.train()
+        print("[INFO] Epoch: {}".format(epoch + 1))
         for i, (input, pred, encodings) in enumerate(train_dataloader):
+            model.train()
+            n_iter += 1
+
             # Make sure batch_size is in the second dim
             input, pred, encodings = (
                 input.permute(1, 0, 2),
@@ -91,7 +117,12 @@ def main(config_path):
             optimizer.zero_grad()
 
             if model_class == Transformer:
-                output = model(input, pos_info=encodings)
+                if has_pos_info:
+                    output = model(
+                        input, pos_info=encodings, token_is_zero=token_is_zero
+                    )
+                else:
+                    output = model(input, pos_info=None, token_is_zero=token_is_zero)
             else:
                 output = model(input)
 
@@ -101,47 +132,117 @@ def main(config_path):
 
             loss_val = loss.item()
             losses.append(loss_val)
+            writer.add_scalar("loss/train_iter", loss_val, n_iter)
+
+            model.eval()
+            with torch.no_grad():
+                # Validation
+                v_losses = []
+                for i, (v_input, v_pred, v_encodings) in enumerate(valid_dataloader):
+                    v_input, v_pred, v_encodings = (
+                        v_input.permute(1, 0, 2),
+                        v_pred.permute(1, 0, 2),
+                        v_encodings.permute(1, 0).unsqueeze(dim=-1),
+                    )
+
+                    v_input, v_pred, v_encodings = (
+                        v_input.to(device),
+                        v_pred.to(device),
+                        v_encodings.to(device),
+                    )
+
+                    if model_class == Transformer:
+                        if has_pos_info:
+                            v_output = model(
+                                v_input,
+                                pos_info=v_encodings,
+                                token_is_zero=token_is_zero,
+                            )
+                        else:
+                            v_output = model(
+                                v_input, pos_info=None, token_is_zero=token_is_zero
+                            )
+                    else:
+                        v_output = model(v_input)
+
+                    if use_diff:
+                        v_output = torch.cumsum(v_output, dim=0)
+                        v_pred = torch.cumsum(v_pred, dim=0)
+
+                    v_loss = criterion(v_output, v_pred)
+                    v_loss_val = v_loss.item()
+                    v_losses.append(v_loss_val)
+
+                valid_loss = np.mean(v_losses)
+
+                writer.add_scalar("loss/valid_iter", valid_loss, n_iter)
+
+                if valid_loss < best_valid_loss:
+                    # Test
+                    t_losses = []
+                    for i, (t_input, t_pred, t_encodings) in enumerate(test_dataloader):
+                        t_input, t_pred, t_encodings = (
+                            t_input.permute(1, 0, 2),
+                            t_pred.permute(1, 0, 2),
+                            t_encodings.permute(1, 0).unsqueeze(dim=-1),
+                        )
+
+                        t_input, t_pred, t_encodings = (
+                            t_input.to(device),
+                            t_pred.to(device),
+                            t_encodings.to(device),
+                        )
+
+                        if model_class == Transformer:
+                            if has_pos_info:
+                                t_output = model(
+                                    t_input,
+                                    pos_info=t_encodings,
+                                    token_is_zero=token_is_zero,
+                                )
+                            else:
+                                t_output = model(
+                                    t_input, pos_info=None, token_is_zero=token_is_zero
+                                )
+                        else:
+                            t_output = model(t_input)
+
+                        if use_diff:
+                            t_output = torch.cumsum(t_output, dim=0)
+                            t_pred = torch.cumsum(t_pred, dim=0)
+
+                        t_loss = criterion(t_output, t_pred)
+                        t_loss_val = t_loss.item()
+                        t_losses.append(t_loss_val)
+
+                    test_loss = np.mean(t_losses)
+
+                    writer.add_scalar("loss/test_iter", test_loss, n_iter)
+
+                    torch.save(
+                        {
+                            "epoch": epoch,
+                            "iter": n_iter,
+                            "model_state_dict": model.state_dict(),
+                            "optimizer_state_dict": optimizer.state_dict(),
+                            "valid_loss": valid_loss,
+                            "test_loss": test_loss,
+                        },
+                        "./model_checkpoints/{}/best/best.pt".format(RUN_NAME),
+                    )
+                    best_valid_loss = valid_loss
+                    best_test_loss = test_loss
+
+        train_loss = np.mean(losses)
+        writer.add_scalar("loss/train_epoch", train_loss, epoch)
 
         scheduler.step()
 
-        writer.add_scalar("loss/train_epoch", np.mean(losses), epoch)
-
-        model.eval()
-        with torch.no_grad():
-            # TODO: Add evaluation metric (@Andrew)
-            v_losses = []
-            for i, (v_input, v_pred, v_encodings) in enumerate(valid_dataloader):
-                v_input, v_pred, v_encodings = (
-                    v_input.permute(1, 0, 2),
-                    v_pred.permute(1, 0, 2),
-                    v_encodings.permute(1, 0).unsqueeze(dim=-1),
-                )
-
-                v_input, v_pred, v_encodings = (
-                    v_input.to(device),
-                    v_pred.to(device),
-                    v_encodings.to(device),
-                )
-
-                if model_class == Transformer:
-                    v_output = model(v_input, pos_info=v_encodings)
-                else:
-                    v_output = model(v_input)
-
-                v_loss = criterion(v_output, v_pred)
-                v_loss_val = v_loss.item()
-                v_losses.append(v_loss_val)
-
-            writer.add_scalar("loss/valid_epoch", np.mean(v_losses), epoch)
-
-        torch.save(
-            {
-                "epoch": epoch,
-                "model_state_dict": model.state_dict(),
-                "optimizer_state_dict": optimizer.state_dict(),
-                "loss": np.mean(losses),
-            },
-            "./model_checkpoints/model_" + config["name"] + ".pt",
+    with open("results.csv", mode="a") as f:
+        f.write(
+            "{},{},{:.4f},{:.4f}\n".format(
+                RUN_NAME, trial_num, best_valid_loss, best_test_loss
+            )
         )
 
 
