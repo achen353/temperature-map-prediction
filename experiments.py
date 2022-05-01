f2d2bf869411d11404ea62ac616769737b2c8e93
achen353 <achen353@gatech.edu>
2022-05-01 19:13:35 -0400

    add experiments

---
+++ experiments.py
@@ -0,0 +1,19 @@
+import os
+from train import main
+
+CONFIGS = [
+    "lstm",
+    "transformer",
+    "transformer_diff",
+    "transformer_enc",
+    "transformer_ulast",
+    "transformer_diff_ulast",
+    "transformer_enc_diff",
+    "transformer_enc_ulast",
+    "transformer_enc_diff_ulast",
+]
+
+for config in CONFIGS:
+    config_path = os.path.join("configs", config + ".json")
+    for n_trial in range(5):
+        main(config_path, n_trial)
